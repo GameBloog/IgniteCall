82d561c852fb215982543ff8150e9f75c22486d2
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2024-01-15 20:38:38 -0300

    Validation with zod

--- src/pages/home/components/ClaimUserNameForm/styles.ts
+++ src/pages/home/components/ClaimUserNameForm/styles.ts
@@ -1,4 +1,5 @@
 import { Box, styled } from "@ignite-ui/react"
+import { Text } from "@ignite-ui/react"
 
 export const Form = styled(Box, {
   display: "grid",
@@ -9,5 +10,12 @@ export const Form = styled(Box, {
 
   "@media(max-width: 600px)": {
     gridTemplateColumns: "1fr",
+  },
+})
+
+export const FormAnnotation = styled("div", {
+  marginTop: "$2",
+  [`> ${Text}`]: {
+    color: "$gray400",
   },
 })
