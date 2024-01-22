7445211bd91b17d647cb543f772fa6eebfa492aa
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2024-01-22 14:50:53 -0300

    Input and Output in the zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@ignite-ui/react"
 import { Container, Header } from "../styles"
 import {
+  FormError,
   IntervalBox,
   IntervalContainer,
   IntervalDay,
@@ -18,8 +19,50 @@ import { ArrowRight } from "phosphor-react"
 import { Controller, useFieldArray, useForm } from "react-hook-form"
 import { z } from "zod"
 import { getWeekDays } from "@/utils/get-week-days"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { convertTimeStringToMinutes } from "@/utils/convert-time-string-to-minutes"
+import { ConnectItem } from "../connect-calendar/styles"
 
-const timeIntervalsFormsSchema = z.object({})
+const timeIntervalsFormsSchema = z.object({
+  Intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      })
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: "Você precisa selecionar pelo menos um dia da semana!",
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes
+        )
+      },
+      {
+        message:
+          "O horário de término deve ser pelo menos 1h distante do início",
+      }
+    ),
+})
+
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormsSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormsSchema>
 
 export default function TimeIntervals() {
   const {
@@ -28,7 +71,8 @@ export default function TimeIntervals() {
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
+    resolver: zodResolver(timeIntervalsFormsSchema),
     defaultValues: {
       Intervals: [
         {
@@ -92,7 +136,9 @@ export default function TimeIntervals() {
 
   const intervals = watch("Intervals")
 
-  async function handleSetTimeIntervals() {}
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+  }
 
   return (
     <Container>
@@ -151,7 +197,11 @@ export default function TimeIntervals() {
           })}
         </IntervalContainer>
 
-        <Button type="submit">
+        {errors.Intervals && (
+          <FormError>{errors.Intervals.root?.message}</FormError>
+        )}
+
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo <ArrowRight />
         </Button>
       </IntervalBox>
--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react"
+import { Box, Text, styled } from "@ignite-ui/react"
 
 export const IntervalBox = styled(Box, {
   marginTop: "$6",
@@ -38,3 +38,8 @@ export const IntervalInput = styled("div", {
     filter: "invert(100%) brightness(30%)",
   },
 })
+
+export const FormError = styled(Text,{
+  color:'#f75a68',
+  marginBottom:'$4'
+}) 
