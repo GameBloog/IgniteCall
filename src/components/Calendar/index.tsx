a0bc8de26d20c38a9d4d5f4de50fa34f0d53ebee
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2024-01-23 13:09:13 -0300

    visual calendar

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,62 @@
+import { CaretLeft, CaretRight } from "phosphor-react"
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from "./styles"
+import { getWeekDays } from "@/utils/get-week-days"
+
+export function Calendar() {
+  const getShortWeekdays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Janeiro <span>2024</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            {" "}
+            <CaretLeft />
+          </button>
+          <button>
+            {" "}
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {getShortWeekdays.map((weekday) => (
+              <th key={weekday}>{weekday}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
